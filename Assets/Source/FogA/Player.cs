19ddcf30cb9d8847e42bd9336115c5e7ec0f7042
oojjrs <oojjrs@hanmail.net>
2020-06-27 22:40:22 +0900

    연습용

---
+++ Assets/Source/FogA/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Script.FogA
+{
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(PlayerInput))]
+    public class Player : MonoBehaviour
+    {
+        public float _speed;
+        private bool _moving;
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _moving = context.performed;
+        }
+
+        private void Update()
+        {
+            if (_moving)
+                GetComponent<CharacterController>().Move(GetComponent<PlayerInput>().actions["Move"].ReadValue<Vector2>() * Time.deltaTime * _speed);
+        }
+    }
+}
---
+++ Assets/Source/FogB/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Script.FogB
+{
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(PlayerInput))]
+    public class Player : MonoBehaviour
+    {
+        public float _speed;
+        private bool _moving;
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _moving = context.performed;
+        }
+
+        private void Update()
+        {
+            if (_moving)
+            {
+                var v = GetComponent<PlayerInput>().actions["Move"].ReadValue<Vector2>();
+                GetComponent<CharacterController>().Move(new Vector3(v.x, 0, v.y) * Time.deltaTime * _speed);
+            }
+        }
+    }
+}
